cdddaa3b1a918b4f35689842960a220d7bbdb959
dev Baal <devbaal@devs-MacBook-Pro.local>
2023-06-20 18:05:22 +0100

    small changes, added funtions and js script

--- app.py
+++ app.py
@@ -4,23 +4,39 @@ import requests
 
 main_app = Flask(__name__)
 
-league_url = "https://api-football-v1.p.rapidapi.com/v3/leagues"
-
-team_url = "https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2021"
-
 headers = {
 	"X-RapidAPI-Key": "8e166509famsh4c22546da213b07p164dcajsnd93994744682",
-	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
-querystring = {"league":"39", "season":"2022"}
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"}
 
-league_data = requests.get(league_url, headers=headers, params={"type":"league"}).json()
 
-team_data = requests.get(team_url, headers=headers, params=querystring).json()
+
+
+def get_league_names_and_id():
+    url = "https://api-football-v1.p.rapidapi.com/v3/leagues"
+
+    league_data = requests.get(url, headers=headers, params={"type":"league"}).json()['response']
+    league_name_id = {}
+    for league in league_data:
+        league_name_id.update({league['league']['name']: league['league']['id']})
+    
+
+    return league_name_id
+
+
+
+def get_team_names(league, season):
+    url = "https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2021"
+    querystring = {league, season}
+
+    team_data = requests.get(url, headers=headers, params=querystring).json()
+
+
+
+
 
 @main_app.route('/')
 def home():
-    return render_template('index.html', data = team_data, league_data=league_data)
+    return render_template('index.html', league_data=get_league_names_and_id())
 
 if __name__ == "__main__":
     main_app.run(debug=True)
